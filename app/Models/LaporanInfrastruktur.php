239117b5328081404f5a95792e70cd7402f734d9
FerianggaArkaanP <herosuprayetno22@gmail.com>
2026-04-28 11:08:08 +0700

    SIGAP-19
    Menangani upload gambar ke storage publik dan men-generate tracking ID.

---
+++ app/Models/LaporanInfrastruktur.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LaporanInfrastruktur extends Model
+{
+    protected $table = 'laporan_infrastruktur';
+
+    protected $guarded = [];
+
+    public function daerah()
+    {
+        return $this->belongsTo(Daerah::class, 'id_daerah');
+    }
+
+    public function analisisAi()
+    {
+        return $this->hasOne(AnalisisAi::class, 'id_laporan');
+    }
+
+    public function pengajuanDana()
+    {
+        return $this->hasMany(PengajuanDana::class, 'id_laporan');
+    }
+}
